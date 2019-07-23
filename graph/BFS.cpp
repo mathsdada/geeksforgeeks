d790a431509f136a9f57d52ea6f73e2f02e1152b
Gangadhar V <vgangadh@hyd-lnxbld297.qualcomm.com>
2019-07-23 15:12:58 +0530

    Graph: BFS & DFS

---
+++ graph/BFS.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <list>
+using namespace std;
+
+class Graph {
+    int numVertices;
+    list<int> *adj;
+public:
+    Graph(int numVertices);
+    void addEdge(int v, int e);
+    void BFS(int v);
+};
+
+Graph::Graph(int vertices) {
+    numVertices = vertices;
+    adj = new list<int>[vertices];
+}
+
+void Graph::addEdge(int v, int e) {
+    adj[v].push_back(e);
+}
+
+void Graph::BFS(int v) {
+    bool *visited = new bool[numVertices];
+    for (int i=0; i<numVertices; ++i) visited[i] = false;
+    
+    list<int> queue;
+    visited[v] = true;
+    queue.push_back(v);
+    
+    while (!queue.empty()) {
+        int v = queue.front();
+        cout << " " << v; 
+        queue.pop_front();
+        
+        for (list<int>::iterator i = adj[v].begin(); i != adj[v].end(); ++i) {
+            if (!visited[*i]) {
+                visited[*i] = true;
+                queue.push_back(*i);
+            }
+        }
+    }
+}
+
+int main() {
+	// your code goes here
+	Graph g(7);
+	g.addEdge(0, 1); 
+    g.addEdge(0, 2); 
+    g.addEdge(1, 3); 
+    g.addEdge(4, 1); 
+    g.addEdge(6, 4); 
+    g.addEdge(5, 6); 
+    g.addEdge(5, 2); 
+    g.addEdge(6, 0);
+    
+    g.BFS(5);
+	return 0;
+}
---
+++ graph/DFS.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <list>
+using namespace std;
+
+class Graph {
+    int numVertices;
+    list<int> *adj;
+    void DFSUtil(int v, bool visited[]);
+public:
+    Graph(int numVertices);
+    void addEdge(int v, int e);
+    void DFS(int v);
+};
+
+Graph::Graph(int vertices) {
+    numVertices = vertices;
+    adj = new list<int>[vertices];
+}
+
+void Graph::addEdge(int v, int e) {
+    adj[v].push_back(e);
+}
+
+void Graph::DFSUtil(int v, bool visited[]) {
+    cout << " " << v;
+    visited[v] = true;
+    
+    for (list<int>::iterator i = adj[v].begin(); i != adj[v].end(); ++i) {
+        if (!visited[*i]) {
+            DFSUtil(*i, visited);
+        }
+    }
+    
+}
+
+void Graph::DFS(int v) {
+    bool *visited = new bool[numVertices];
+    for (int i=0; i<numVertices; ++i) visited[i] = false;
+    
+    DFSUtil(v, visited);
+}
+
+int main() {
+	// your code goes here
+	Graph g(7);
+	g.addEdge(0, 1); 
+    g.addEdge(0, 2); 
+    g.addEdge(1, 3); 
+    g.addEdge(4, 1); 
+    g.addEdge(6, 4); 
+    g.addEdge(5, 6); 
+    g.addEdge(5, 2); 
+    g.addEdge(6, 0);
+    
+    g.DFS(5);
+	return 0;
+}
